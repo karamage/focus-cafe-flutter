bddfb51e0b012e9cc4225ac33a1bf4856cbefd26
karamage <karamage.umauma@gmail.com>
2021-09-02 08:47:00 +0900

    add app state

--- lib/data/datasource/firebase_datasource.dart
+++ lib/data/datasource/firebase_datasource.dart
@@ -4,8 +4,10 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:focus_cafe_flutter/data/datasource/remote_datasource.dart';
 
 class FirebaseDatasource implements RemoteDatasource {
+  /* ここはinitializeApp読んだあとに遅らせる必要あり lateinit だっけ
   FirebaseFirestore _db = FirebaseFirestore.instance;
   FirebaseAuth _auth = FirebaseAuth.instance;
+   */
 
   @override
   initializeApp() async {
