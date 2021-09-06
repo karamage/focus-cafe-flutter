7e08d75904deb50761aed37918a69e53ba7064b0
karamage <karamage.umauma@gmail.com>
2021-09-06 17:10:39 +0900

    watch appstate

--- lib/ui/screens/main_screen.dart
+++ lib/ui/screens/main_screen.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:focus_cafe_flutter/data/models/app_state_type.dart';
 import 'package:focus_cafe_flutter/data/providers/app_state_provider.dart';
+import 'package:focus_cafe_flutter/ui/screens/splash_screen.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class MainScreen extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final appState = ref.watch(appStateProvider);
-    /*
-    if (appState.stateType == AppStateType.initCompleted) {
-      setLoginState();
-    }
+    useEffect((){
+      if (appState.stateType == AppStateType.initCompleted) {
+        setLoginState(ref);
+      }
+    }, [appState.stateType]);
     switch (appState.stateType) {
       case AppStateType.init:
         return SplashScreen();
@@ -18,21 +21,23 @@ class MainScreen extends HookConsumerWidget {
         return SplashScreen(); //TODO
       case AppStateType.walkthrough:
         return SplashScreen(); //TODO
-      case AppStateType.login:
-        return LoginScreen();
-      case AppStateType.loginCompleted:
-        return HomeScreen();
+      //case AppStateType.login:
+      //  return LoginScreen();
+      //case AppStateType.loginCompleted:
+      //  return HomeScreen();
     }
-     */
     return Scaffold(
       body: Center(
-          child: Text('Main')),
+          child: Text('Another ${appState.stateType}')),
     );
   }
 
-  setLoginState() async {
+  setLoginState(WidgetRef ref) async {
+    final appStateViewModel = ref.read(appStateProvider.notifier);
+    WidgetsBinding.instance!.addPostFrameCallback((timeStamp) {
+      appStateViewModel.setAppStateType(AppStateType.login);
+    });
     /*
-    final appStateViewModel = useProvider(appStateProvider);
     final auth = useProvider(authProvider);
     final myUserViewModel = useProvider(myUserProvider);
     final userId = await myUserViewModel.getMyUserId();
@@ -42,6 +47,6 @@ class MainScreen extends HookConsumerWidget {
     } else {
       appStateViewModel.setAppStateType(AppStateType.login);
     }
-     */
+    */
   }
 }
