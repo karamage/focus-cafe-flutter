a3a91d59690d710965d15f9d8ea24af45d54cbb5
karamage <karamage.umauma@gmail.com>
2021-09-10 16:54:27 +0900

    add user datasource.

--- lib/data/datasource/firebase_datasource.dart
+++ lib/data/datasource/firebase_datasource.dart
@@ -4,6 +4,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:focus_cafe_flutter/data/datasource/remote_datasource.dart';
 import 'package:focus_cafe_flutter/util/constants.dart';
 
+const USERS_PATH = "users";
+
 class FirebaseDatasource implements RemoteDatasource {
   late FirebaseFirestore _db = FirebaseFirestore.instance;
   late FirebaseAuth _auth = FirebaseAuth.instance;
@@ -22,4 +24,45 @@ class FirebaseDatasource implements RemoteDatasource {
     map[ID_KEY] = firebaseUser.uid;
     return map;
   }
+
+  @override
+  Future<Map<String, dynamic>?> getUser(String userId) async {
+    DocumentReference doc = _db.collection(USERS_PATH).doc(userId);
+    return convertTimestamp((await doc.get()).data() as Map<String, dynamic>?);
+  }
+
+  @override
+  Future<Map<String, dynamic>?> addUser(Map<String, dynamic> params) async {
+    DocumentReference doc = _db.collection(USERS_PATH).doc(params[ID_KEY]);
+    DocumentSnapshot snapshot = await doc.get();
+    if (!snapshot.exists) {
+      _setCreatedAtParam(params);
+      _setUpdatedAtParam(params);
+      await doc.set(params, SetOptions(merge: true));
+      snapshot = await doc.get();
+    }
+    return convertTimestamp(snapshot.data() as Map<String, dynamic>?);
+  }
+
+  Map<String, dynamic>? convertTimestamp(Map<String, dynamic>? json) {
+    if (json == null) return null;
+    if (json["createdAt"] is Timestamp) {
+      json["createdAt"] = json["createdAt"].toDate().toString();
+    }
+    if (json["updatedAt"] is Timestamp) {
+      json["updatedAt"] = json["updatedAt"].toDate().toString();
+    }
+    return json;
+  }
+
+  FieldValue _serverTimestamp() => FieldValue.serverTimestamp();
+  Map<String, dynamic> _setCreatedAtParam(Map<String, dynamic> params) {
+    params["createdAt"] = _serverTimestamp();
+    return params;
+  }
+  Map<String, dynamic> _setUpdatedAtParam(Map<String, dynamic> params) {
+    params["updatedAt"] = _serverTimestamp();
+    return params;
+  }
+
 }
--- lib/data/datasource/remote_datasource.dart
+++ lib/data/datasource/remote_datasource.dart
@@ -1,4 +1,6 @@
 abstract class RemoteDatasource {
   initializeApp();
   Future<Map<String, dynamic>?> signInAnonymously();
+  Future<Map<String, dynamic>?> getUser(String userId);
+  Future<Map<String, dynamic>?> addUser(Map<String, dynamic> params);
 }
