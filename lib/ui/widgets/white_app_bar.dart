d26e29fb10881527a8c03bc7390a94e50a469ccf
karamage <karamage.umauma@gmail.com>
2021-09-14 08:47:37 +0900

    add white app bar.

---
+++ lib/ui/widgets/white_app_bar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WhiteAppBar {
+  static Widget build(String title, [List<Widget>? actions]) {
+    return AppBar(
+      title: Text(title,
+          style: TextStyle(color: Colors.grey)
+      ),
+      backgroundColor: Colors.white,
+      brightness: Brightness.light,
+      actions: actions,
+      iconTheme: IconThemeData(color: Colors.grey),
+    );
+  }
+}
