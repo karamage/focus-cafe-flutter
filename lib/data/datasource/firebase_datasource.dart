13d115deff6a48477ceecfe572c44e7e382339d7
karamage <karamage.umauma@gmail.com>
2021-09-17 09:15:23 +0900

    add getDones.

--- lib/data/datasource/firebase_datasource.dart
+++ lib/data/datasource/firebase_datasource.dart
@@ -5,6 +5,7 @@ import 'package:focus_cafe_flutter/data/datasource/remote_datasource.dart';
 import 'package:focus_cafe_flutter/util/constants.dart';
 
 const USERS_PATH = "users";
+const DONES_PATH = "dones";
 
 class FirebaseDatasource implements RemoteDatasource {
   late FirebaseFirestore _db = FirebaseFirestore.instance;
@@ -46,6 +47,23 @@ class FirebaseDatasource implements RemoteDatasource {
     return convertTimestamp(snapshot.data() as Map<String, dynamic>?);
   }
 
+  // --- private method ---
+  DocumentReference _getUserRef(uuid) => _db.collection(USERS_PATH).doc(uuid);
+  //DocumentReference _getItemRef(uuid) => _db.collection(ITEMS_PATH).doc(uuid);
+  Future<DocumentSnapshot> _getUserDoc(uuid) => _getUserRef(uuid).get();
+  //Future<DocumentSnapshot> _getItemDoc(itemId) => _getItemRef(itemId).get();
+
+  Future<List<Map<String, dynamic>>> _getJsons(Query q) async {
+    final list = (await q.get()).docs.map((doc) =>
+        _getJson(doc.data() as Map<String, dynamic>?))
+        .toList();
+    list.removeWhere((value) => value == null);
+    return list as List<Map<String, dynamic>>;
+  }
+
+  Map<String, dynamic>? _getJson(Map<String, dynamic>? json) =>
+      convertTimestamp(json);
+
   Map<String, dynamic>? convertTimestamp(Map<String, dynamic>? json) {
     if (json == null) return null;
     if (json["createdAt"] is Timestamp) {
@@ -65,6 +83,17 @@ class FirebaseDatasource implements RemoteDatasource {
   Map<String, dynamic> _setUpdatedAtParam(Map<String, dynamic> params) {
     params["updatedAt"] = _serverTimestamp();
     return params;
+  }
+
+  Query _getOurDonesQuery(DateTime? lastDate, int limit) {
+    Query query = _db.collection(DONES_PATH)
+        .orderBy("endDate", descending: true);
+    return _getPagingQuery(query, lastDate, limit);
+  }
+
+  Query _getPagingQuery(Query q, DateTime? lastDate, int limit) {
+    q = lastDate != null ? q.startAfter([Timestamp.fromDate(lastDate)]) : q;
+    return q.limit(limit);
   }
 
 }
