739a409d16f285c1e22baf3d2030a0497cdf78e3
karamage <karamage.umauma@gmail.com>
2021-09-25 14:22:53 +0900

    start timer.

--- lib/data/providers/focus_time_provider.dart
+++ lib/data/providers/focus_time_provider.dart
@@ -1,6 +1,7 @@
+import 'package:focus_cafe_flutter/data/models/focus_time.dart';
 import 'package:focus_cafe_flutter/ui/notifiers/focus_time_notifier.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-final focusTimeProvider = StateNotifierProvider((ref) =>
+final focusTimeProvider = StateNotifierProvider<FocusTimeNotifier, FocusTime>((ref) =>
     FocusTimeNotifier()
 );
--- lib/ui/screens/timer_screen.dart
+++ lib/ui/screens/timer_screen.dart
@@ -1,26 +1,48 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:focus_cafe_flutter/data/models/focus_time.dart';
+import 'package:focus_cafe_flutter/data/providers/focus_time_provider.dart';
 import 'package:focus_cafe_flutter/data/providers/my_user_provider.dart';
+import 'package:focus_cafe_flutter/ui/notifiers/focus_time_notifier.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class TimerScreen extends HookConsumerWidget {
+  Timer? _timer;
+  FocusTime? _focusTime;
+  FocusTimeNotifier? _focusTimeNotifier;
+
+  void _onTimer(Timer timer) {
+    _focusTimeNotifier?.setRemainingTime((_focusTime?.remainingTime ?? 0) - 1);
+  }
+
+  void startTimer() {
+    _timer = Timer.periodic(const Duration(seconds: 1), _onTimer);
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final myUser = ref.watch(myUserProvider);
     final _notifier = ref.read(myUserProvider.notifier);
+    _focusTime = ref.watch(focusTimeProvider);
+    _focusTimeNotifier = ref.read(focusTimeProvider.notifier);
     useEffect((){
       _notifier.reload();
+      WidgetsBinding.instance!.addPostFrameCallback((timeStamp) {
+        _focusTimeNotifier?.setRemainingTime(60);
+      });
       return null;
     }, []);
     return Center(
       child: Column(
         children: [
           Text("Timer myUser id = ${myUser.id} name=${myUser.name}"),
-          Text("Timer 00:00"),
+          Text("Timer ${_focusTime?.remainingTime}"),
           ElevatedButton(
             child: Text('集中する'),
             onPressed: () {
-              // TODO
+              startTimer();
             },
           ),
         ],
