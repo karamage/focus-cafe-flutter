550485335ca2d62ef62a80edd6fe7ddc0508d352
karamage <karamage.umauma@gmail.com>
2021-09-26 16:54:31 +0900

    add focus notifier.

---
+++ lib/ui/notifiers/focus_notifier.dart
@@ -0,0 +1,14 @@
+import 'package:focus_cafe_flutter/data/models/focus.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class FocusNotifier extends StateNotifier<Focus> {
+  FocusNotifier() : super(const Focus()) {}
+
+  void setFocusTime(int focusTime) {
+    state = state.copyWith(focusTime: focusTime);
+  }
+
+  void setIsFocus(bool isFocus) {
+    state = state.copyWith(isFocus: isFocus);
+  }
+}
