7043e1d20ce0bef5ed92e4a25bac4dc3f9f604e6
karamage <karamage.umauma@gmail.com>
2021-10-10 11:19:51 +0900

    add addDone to repository.

--- lib/data/repository/done_repository.dart
+++ lib/data/repository/done_repository.dart
@@ -1,5 +1,17 @@
 import 'package:focus_cafe_flutter/data/models/done.dart';
+import 'package:focus_cafe_flutter/data/models/user.dart';
 
 abstract class DoneRepository {
   Future<List<Done>> getOurDones(Done? lastItem, int limit);
+  Future<Done?> addDone(
+    DateTime startDate,
+    DateTime endDate,
+    int totalElapsedTime,
+    User user,
+    String body,
+    [
+      String? questId,
+      String? questTitle,
+    ]
+  );
 }
--- lib/data/repository/done_repository_impl.dart
+++ lib/data/repository/done_repository_impl.dart
@@ -1,5 +1,6 @@
 import 'package:focus_cafe_flutter/data/datasource/remote_datasource.dart';
 import 'package:focus_cafe_flutter/data/models/done.dart';
+import 'package:focus_cafe_flutter/data/models/user.dart';
 import 'package:focus_cafe_flutter/data/repository/done_repository.dart';
 
 class DoneRepositoryImpl implements DoneRepository {
@@ -10,5 +11,22 @@ class DoneRepositoryImpl implements DoneRepository {
   Future<List<Done>> getOurDones(Done? lastItem, int limit) async {
     final jsons = await _ds.getOurDones(lastItem?.endDate, limit);
     return jsons.map((json) => Done.fromJson(json)).toList();
+  }
+
+  @override
+  Future<Done?> addDone(
+    DateTime startDate,
+    DateTime endDate,
+    int totalElapsedTime,
+    User user,
+    String body,
+    [
+      String? questId,
+      String? questTitle,
+    ]
+  ) async {
+    final params = Done.createParams(startDate, endDate, totalElapsedTime, user, body, questId, questTitle);
+    final json = await _ds.addDone(params);
+    return json != null ? Done.fromJson(json):null;
   }
 }
