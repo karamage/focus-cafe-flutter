8cb30b24482b9bf002f138a8ec063620b172656b
karamage <karamage.umauma@gmail.com>
2021-10-11 11:05:24 +0900

    add EditDoneScreen.

--- lib/ui/screens/edit_done_screen.dart
+++ lib/ui/screens/edit_done_screen.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:focus_cafe_flutter/data/models/done.dart';
 import 'package:focus_cafe_flutter/ui/widgets/white_app_bar.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class EditDoneScreen extends HookConsumerWidget {
+  final Done editDone;
+  EditDoneScreen({Key? key, required this.editDone}): super(key: key);
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: WhiteAppBar.build("集中が完了しました"),
       body: Center(
-        child: Text("Edit Done"),
+        child: Text("Edit Done id=${editDone.id}"),
       ),
     );
   }
