1834d658720000d38a5463849b808ebd1b0ee021
karamage <karamage.umauma@gmail.com>
2021-10-14 08:43:17 +0900

    add activity model.

---
+++ lib/data/models/activity.dart
@@ -0,0 +1,13 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'activity.freezed.dart';
+part 'activity.g.dart';
+
+@freezed
+abstract class Activity with _$Activity{
+  const factory Activity({
+    @Default([]) List<Activity> dates,
+    @Default(0) int continuedDateNum,
+  }) = _Activity;
+  factory Activity.fromJson(Map<String, dynamic> json) => _$ActivityFromJson(json);
+}
