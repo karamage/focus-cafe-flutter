be86c2d33ab271c730f5c92f7b687286ca9a2974
karamage <karamage.umauma@gmail.com>
2021-10-15 15:50:49 +0900

    add activity to firebase.

--- lib/data/datasource/firebase_datasource.dart
+++ lib/data/datasource/firebase_datasource.dart
@@ -7,6 +7,7 @@ import 'package:focus_cafe_flutter/util/local_storage_manager.dart';
 
 const USERS_PATH = "users";
 const DONES_PATH = "dones";
+const ACTIVITYS_PATH = "activitys";
 
 class FirebaseDatasource implements RemoteDatasource {
   late FirebaseFirestore _db = FirebaseFirestore.instance;
@@ -75,6 +76,19 @@ class FirebaseDatasource implements RemoteDatasource {
     return await _getJsons(_getOurDonesQuery(lastDate, limit));
   }
 
+  @override
+  Future<Map<String, dynamic>?> getActivity(String userId) async {
+    DocumentReference doc = _db.collection(ACTIVITYS_PATH).doc(userId);
+    return convertTimestamp((await doc.get()).data() as Map<String, dynamic>?);
+  }
+
+  @override
+  Future<Map<String, dynamic>?> addActivity(Map<String, dynamic> params) async {
+    params = await _setBasicParams(params);
+    return convertTimestamp(
+        await _setDocument(ACTIVITYS_PATH, params[ID_KEY], params));
+  }
+
   // --- private method ---
   DocumentReference _getUserRef(uuid) => _db.collection(USERS_PATH).doc(uuid);
   //DocumentReference _getItemRef(uuid) => _db.collection(ITEMS_PATH).doc(uuid);
@@ -103,6 +117,9 @@ class FirebaseDatasource implements RemoteDatasource {
     }
     if (json["updatedAt"] is Timestamp) {
       json["updatedAt"] = json["updatedAt"].toDate().toString();
+    }
+    if (json["dates"] is List<Timestamp>) {
+      json["dates"] = json["dates"].map((datetime) => datetime.toDate().toString());
     }
     return json;
   }
--- lib/data/datasource/remote_datasource.dart
+++ lib/data/datasource/remote_datasource.dart
@@ -7,4 +7,6 @@ abstract class RemoteDatasource {
   Future<List<Map<String, dynamic>>> getOurDones(DateTime? lastDate, int limit);
   Future<Map<String, dynamic>?> addDone(Map<String, dynamic> params);
   Future<Map<String, dynamic>?> editDone(Map<String, dynamic> params);
+  Future<Map<String, dynamic>?> getActivity(String userId);
+  Future<Map<String, dynamic>?> addActivity(Map<String, dynamic> params);
 }
