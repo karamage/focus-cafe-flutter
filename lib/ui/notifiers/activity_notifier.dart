d7f45566229d3150091d189e34095361c6c00778
karamage <karamage.umauma@gmail.com>
2021-10-16 15:46:43 +0900

    add activity to notifier.

---
+++ lib/ui/notifiers/activity_notifier.dart
@@ -0,0 +1,34 @@
+import 'package:focus_cafe_flutter/data/models/activity.dart';
+import 'package:focus_cafe_flutter/data/repository/activity_repository.dart';
+import 'package:focus_cafe_flutter/util/local_storage_manager.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class ActivityNotifier extends StateNotifier<Activity> {
+  ActivityNotifier({required ActivityRepository repository})
+      : _repository = repository,
+        super(const Activity()) {}
+  final ActivityRepository _repository;
+
+  Future<void> reload() async {
+    final myUserId = await LocalStorageManager.getMyUserId();
+    if (myUserId != null) {
+      final activity = await _repository.getActivity(myUserId);
+      if (activity != null) state = activity;
+    }
+  }
+
+  Future<Activity?> addActivity(DateTime addDate) async {
+    final myUserId = await LocalStorageManager.getMyUserId();
+    if (myUserId == null) return null;
+    final preActivity = await _repository.getActivity(myUserId);
+    final dates = preActivity?.dates ?? [];
+    dates.add(addDate);
+    final activity = await _repository.updateActivity(myUserId, dates);
+    if (activity != null) {
+      state = activity;
+      return state.copyWith();
+    }
+    return null;
+  }
+
+}
