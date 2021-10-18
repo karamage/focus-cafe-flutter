7e0ed6c41cbb4647adda3f13edf1b33a43c8bde3
karamage <karamage.umauma@gmail.com>
2021-10-18 11:58:58 +0900

    add addRestUserParams

--- lib/data/models/rest_user.dart
+++ lib/data/models/rest_user.dart
@@ -16,4 +16,19 @@ abstract class RestUser with _$RestUser {
 
   factory RestUser.fromJson(Map<String, dynamic> json) =>
       _$RestUserFromJson(json);
+
+  // User作成の際のパラメータ
+  static Map<String, dynamic> addRestUserParams(
+    DateTime startDate,
+    User user,
+  ) {
+    Map<String, dynamic> map = Map();
+    map["startDate"] = startDate;
+    map["user"] = User.getSubUserParams(user);
+    map["chairId"] = null;
+    map["message"] = null;
+    map["messageDate"] = null;
+    map["isOnline"] = true;
+    return map;
+  }
 }
