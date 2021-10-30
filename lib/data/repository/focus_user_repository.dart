e50b4a648bba19995ca026056bb62361987d827a
karamage <karamage.umauma@gmail.com>
2021-10-30 16:59:47 +0900

    add focus_users to repository.

---
+++ lib/data/repository/focus_user_repository.dart
@@ -0,0 +1,8 @@
+import 'package:focus_cafe_flutter/data/models/focus_user.dart';
+import 'package:focus_cafe_flutter/data/models/user.dart';
+
+abstract class FocusUserRepository {
+  Future<FocusUser?> addFocusUser(String id, DateTime startDate, User user, int focusTime, int todayCount);
+  Future<void> deleteFocusUser(String id);
+  Stream<FocusUser> onSnapshotFocusUser();
+}
