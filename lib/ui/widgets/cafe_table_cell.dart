d928bf55e4ad9825241f311fff52f9e1970bb8b8
karamage <karamage.umauma@gmail.com>
2021-11-12 10:03:49 +0900

    add cafe chairs.

--- lib/ui/widgets/cafe_table_cell.dart
+++ lib/ui/widgets/cafe_table_cell.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:focus_cafe_flutter/ui/widgets/cafe_chair.dart';
+import 'package:focus_cafe_flutter/ui/widgets/cafe_table.dart';
 
 class CafeTableCell extends StatelessWidget {
   final Map<String, Object> tableData;
@@ -8,6 +9,7 @@ class CafeTableCell extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final tableId = tableData["id"] as int;
     final chairs = tableData["chairs"]! as List<Map<String, Object>>;
     return Container(
       decoration: BoxDecoration(color: Colors.red),
@@ -15,7 +17,7 @@ class CafeTableCell extends StatelessWidget {
       child: Row(
         children: [
           CafeChair(chairData: chairs[0], imagePath: "TODO", restUser: null),
-          Text("テーブル"),
+          CafeTable(id: tableId),
           CafeChair(chairData: chairs[1], imagePath: "TODO", restUser: null),
         ],
       ),
