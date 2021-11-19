75a2d5dfa5376351dbe503d2b76290f67350e3de
karamage <karamage.umauma@gmail.com>
2021-11-19 16:20:56 +0900

    tweak wrap timer.

--- lib/ui/widgets/wrap_timer.dart
+++ lib/ui/widgets/wrap_timer.dart
@@ -5,7 +5,6 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:timezone/data/latest.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
 
-
 class WrapTimer extends StatefulWidget {
   final bool isStart;
   final int initTime;
