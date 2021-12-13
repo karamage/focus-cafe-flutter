4ab5bb0d2d678949db4755ee57a061243c57c401
karamage <karamage.umauma@gmail.com>
2021-12-13 15:29:55 +0900

    set header logo.

--- lib/ui/widgets/white_app_bar.dart
+++ lib/ui/widgets/white_app_bar.dart
@@ -6,8 +6,8 @@ class WhiteAppBar {
       title: title == "集中タイマー" ? Image.asset(
         'assets/images/header_logo.png',
         fit: BoxFit.contain,
-        width: 180,
-        height: 76,
+        width: 192,
+        height: 82,
       ) :
       Text(title,
         style: TextStyle(color: Colors.grey)
