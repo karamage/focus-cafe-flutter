5d3b1de9ef7f62b89f4c97a98ef88d37446d3c4f
karamage <karamage.umauma@gmail.com>
2021-12-13 15:29:17 +0900

    set header logo.

--- lib/ui/widgets/white_app_bar.dart
+++ lib/ui/widgets/white_app_bar.dart
@@ -3,8 +3,14 @@ import 'package:flutter/material.dart';
 class WhiteAppBar {
   static PreferredSizeWidget build(String title, [List<Widget>? actions]) {
     return AppBar(
-      title: Text(title,
-          style: TextStyle(color: Colors.grey)
+      title: title == "集中タイマー" ? Image.asset(
+        'assets/images/header_logo.png',
+        fit: BoxFit.contain,
+        width: 180,
+        height: 76,
+      ) :
+      Text(title,
+        style: TextStyle(color: Colors.grey)
       ),
       backgroundColor: Colors.white,
       brightness: Brightness.light,
