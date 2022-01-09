7b7ba1857974d94ef23e966635aed957b1dccbe1
karamage <karamage.umauma@gmail.com>
2022-01-09 11:15:21 +0900

    add notification converter.

---
+++ lib/data/converter/firestore/notification_converter.dart
@@ -0,0 +1,41 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:focus_cafe_flutter/data/converter/firestore/common_util.dart';
+import 'package:focus_cafe_flutter/data/models/notification.dart';
+
+DocumentReference<Notification> notificationConverter(DocumentReference doc) {
+  return doc
+      .withConverter<Notification>(
+      fromFirestore: notificationFromFirestore,
+      toFirestore: notificationToFirestore
+  );
+}
+
+Query<Notification> notificationQueryConverter(Query query) {
+  return query
+      .withConverter<Notification>(
+      fromFirestore: notificationFromFirestore,
+      toFirestore: notificationToFirestore
+  );
+}
+
+Notification notificationFromFirestore(
+    DocumentSnapshot<Map<String, dynamic>> snapshot,
+    SnapshotOptions? options,
+    ) {
+  return Notification.fromJson(snapshot.data()!);
+}
+
+Map<String, Object?> notificationToFirestore(
+    Notification model,
+    SetOptions? options,
+    ) {
+  final createdAt = model.createdAt;
+  return {
+    ...model.toJson(),
+    if (model.toUser != null) "toUser": model.toUser?.toJson(),
+    if (model.toUser?.id != null) "userRef": getUserRef(model.toUser?.id),
+    if (model.fromUser != null) "fromUser": model.fromUser?.toJson(),
+    if (createdAt != null) "createdAt": Timestamp.fromDate(createdAt),
+  };
+}
+
