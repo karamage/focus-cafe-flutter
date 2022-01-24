b74534601b7869568073afdb2757dd1f69104dd5
karamage <karamage.umauma@gmail.com>
2022-01-24 16:37:39 +0900

    add notifications list.

--- lib/ui/notifiers/notifications_notifier.dart
+++ lib/ui/notifiers/notifications_notifier.dart
@@ -14,14 +14,30 @@ class NotificationsNotifier extends StateNotifier<Notifications> {
 
   Notification? _lastItem;
   bool _isLast = false;
+  String _userId = "";
   final LIST_LIMIT = 20;
 
+  Future<void> onRefresh() async {
+    state = state.copyWith(isLoading: true);
+    await reload(_userId);
+  }
+
   Future<void> reload(String userId) async {
+    _userId = userId;
     _clear();
     final list = await _getNotifications(userId);
     if (list.length > 0) _lastItem = list.last;
     _isLast = list.length < LIST_LIMIT;
     state = state.copyWith(items: list, isLoading: false);
+  }
+
+  Future<void> next() async {
+    if (state.isLoading == true || _isLast) return;
+    state = state.copyWith(isLoading: true);
+    final list = await _getNotifications(_userId);
+    _lastItem = list.length > 0 ? list.last : null;
+    _isLast = list.length < LIST_LIMIT;
+    state = state.copyWith(items: [...state.items]..addAll(list), isLoading: false);
   }
 
   Future<void> addLikeNotification(
--- lib/ui/screens/notifications_screen.dart
+++ lib/ui/screens/notifications_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:focus_cafe_flutter/data/models/notification.dart' as FocusCafeNotification;
 import 'package:focus_cafe_flutter/data/providers/my_user_provider.dart';
 import 'package:focus_cafe_flutter/data/providers/notifications_provider.dart';
 import 'package:focus_cafe_flutter/ui/notifiers/notifications_notifier.dart';
+import 'package:focus_cafe_flutter/ui/widgets/easy_list_view.dart';
+import 'package:focus_cafe_flutter/ui/widgets/loading_indicator.dart';
+import 'package:focus_cafe_flutter/ui/widgets/notification_cell.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class NotificationsScreen extends HookConsumerWidget {
@@ -14,29 +18,23 @@ class NotificationsScreen extends HookConsumerWidget {
     final myUser = ref.watch(myUserProvider);
     _notifier = ref.read(notificationsProvider.notifier);
     useEffect((){
-      //_notifier.reload();
+      final userId = myUser.id;
+      if (userId != null) {
+        _notifier.reload(userId);
+      }
       // TODO 既読処理
       return null;
     }, []);
-    return Center(
-      child: Text("Notifications List"),
-    );
-    /*
     return EasyListView(
       items: state.items,
       onRefresh: _notifier.onRefresh,
       onNext: _notifier.next,
       buildCells: (items) =>
           items.map((item) =>
-              DoneCell(
-                done: item as Done,
-                myUserId: myUser.id ?? "",
-                tapLike: tapLike,
-              )
+            NotificationCell(item: item as FocusCafeNotification.Notification)
           ).toList(),
       isLoading: state.isLoading,
       loading: LoadingIndicator(),
     );
-     */
   }
 }
