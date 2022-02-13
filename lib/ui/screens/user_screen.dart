b72160540cd7008186f6e9fef556ecc7ec27a7f7
karamage <karamage.umauma@gmail.com>
2022-02-13 16:18:11 +0900

    add user notifier and provider.

--- lib/ui/screens/user_screen.dart
+++ lib/ui/screens/user_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:focus_cafe_flutter/data/providers/user_provider.dart';
 import 'package:focus_cafe_flutter/ui/widgets/white_app_bar.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -8,10 +10,15 @@ class UserScreen extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final user = ref.watch(userProvider(userId));
+    final _userNotifier = ref.read(userProvider(userId).notifier);
+    useEffect((){
+      _userNotifier.reload();
+    }, []);
     return Scaffold(
       appBar: WhiteAppBar.build("プロフィール", null),
       body: Center(
-        child: Text("User Screen"),
+        child: Text("User Screen ${user.name}"),
       ),
     );
   }
