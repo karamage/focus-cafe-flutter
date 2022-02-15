b498c81f2971916f2cb989bdf0d55a24b8357d99
karamage <karamage.umauma@gmail.com>
2022-02-16 08:53:10 +0900

    add user screen.

--- lib/ui/screens/user_screen.dart
+++ lib/ui/screens/user_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:focus_cafe_flutter/data/models/done.dart';
 import 'package:focus_cafe_flutter/data/providers/user_dones_provider.dart';
 import 'package:focus_cafe_flutter/data/providers/user_provider.dart';
+import 'package:focus_cafe_flutter/ui/widgets/done_cell.dart';
+import 'package:focus_cafe_flutter/ui/widgets/easy_list_view.dart';
+import 'package:focus_cafe_flutter/ui/widgets/loading_indicator.dart';
 import 'package:focus_cafe_flutter/ui/widgets/white_app_bar.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -19,11 +23,31 @@ class UserScreen extends HookConsumerWidget {
       _userNotifier.reload();
       _donesNotifier.reload();
     }, []);
+    final tapLike = (String itemId) async {
+      //_notifier.addLike(itemId, myUser);
+    };
     return Scaffold(
       appBar: WhiteAppBar.build("プロフィール", null),
-      body: Center(
-        child: Text("User Screen ${user.name} items.length=${dones.items.length}"),
-      ),
+      body: EasyListView(
+          items: dones.items,
+          onRefresh: _donesNotifier.onRefresh,
+          onNext: _donesNotifier.next,
+          header: Container(
+            padding: EdgeInsets.all(30.0),
+            child: Center(child: Text("${user.name}")),
+          ),
+          buildCells: (items) =>
+              items.map((item) =>
+                  DoneCell(
+                    done: item as Done,
+                    myUserId: user.id ?? "",
+                    tapLike: tapLike,
+                    //tapComment: tapComment,
+                  )
+              ).toList(),
+          isLoading: dones.isLoading,
+          loading: LoadingIndicator(),
+        )
     );
   }
 }
