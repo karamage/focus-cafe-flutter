9e2ce4dc897e196b2a58b30fb0da06e05f3e4484
karamage <karamage.umauma@gmail.com>
2022-03-02 09:08:44 +0900

    add block user repository.

---
+++ lib/data/repository/block_user_repository.dart
@@ -0,0 +1,7 @@
+import 'package:focus_cafe_flutter/data/models/block_user.dart';
+import 'package:focus_cafe_flutter/data/models/user.dart';
+
+abstract class BlockUserRepository {
+  Future<BlockUser?> addBlockUser(String userId, User blockUser);
+  Future<List<BlockUser>> getBlockUsers(String userId, DateTime? lastDate, int limit);
+}
